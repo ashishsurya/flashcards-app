b596512debced3ea384faa85a0fae87d927bc45d
Surya Ashish <ashishsurya126@gmail.com>
2023-03-31 19:07:14 +0530

    configured some runtime errors

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -11,6 +14,6 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.star.findMany();
   }),
 });
